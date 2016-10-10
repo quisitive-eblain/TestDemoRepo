9bfecd5fb02ffa7390ffd8719733540348be042a
rbasw-eblain <eli.blain@quisitive.com>
2016-10-10 09:33:16 -0500

    initial commit with demo integration test for azuretestapi

---
+++ TestDemo/TestAzureAPIIntegrationTest/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestAzureAPIIntegrationTest
+{
+    public class Class1
+    {
+        private string urlBase = @"https://quisitivetestazureapi.azurewebsites.net/";
+
+        [Fact]
+        public async Task Get_Test()
+        {
+            var url = $"{urlBase}api/values";
+            HttpClient client = new HttpClient();
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Get,
+            };
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.SendAsync(request);
+            //var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ TestDemo/TestAzureAPIIntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAzureAPIIntegrationTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cfb4a1e-5c2c-4a93-82ec-48bd0613d902")]
